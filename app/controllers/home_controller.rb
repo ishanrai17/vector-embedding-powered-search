60b649ae15cefb80e59157619bbec53b907f0b90
Ishan Rai <ishanrai@Ishans-MacBook-Pro.local>
2025-01-26 18:58:27 -0500

    Application controller

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,29 @@
+class HomeController < ApplicationController
+  def index
+    # render page views/home/index.html
+    render file: 'app/views/home/index.html', layout: false
+  end
+
+  def search
+    # Cosine similarity search
+    # results = [1,2,3,4]
+    results = perform_search(params[:query])
+    render json: results
+  end
+
+  private
+
+  def perform_search(query)
+    embedding = Embedder.MiniLM(query)
+    pg_embedding = "ARRAY[#{embedding.join(',')}]::vector"
+    ActiveRecord::Base.connection.execute(<<-SQL
+      SELECT question, answer, 
+            1 - (embedding <=> #{pg_embedding}) AS similarity
+      FROM qas
+      WHERE 1 - (embedding <=> #{pg_embedding}) > 0.7
+      ORDER BY similarity DESC
+      LIMIT 5;
+      SQL
+    )
+  end
+end
