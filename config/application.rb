696cdc912021dd314e0f6192e0512bc1e17557be
Ishan Rai <ishanrai@Ishans-MacBook-Pro.local>
2025-01-26 18:58:59 -0500

    Config files

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative "boot"
 
 require "rails/all"
 
+require 'dotenv/rails-now'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -15,6 +17,10 @@ module App
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+
+    config.active_job.queue_adapter = :sidekiq
+
+    Dotenv::Railtie.load
 
     # Configuration for the application, engines, and railties goes here.
     #
